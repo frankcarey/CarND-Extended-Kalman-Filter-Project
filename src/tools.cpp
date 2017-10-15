afb5613081e752c3d9232962a20852b4259fb134
Frank Carey <fcarey@gmail.com>
2017-10-15 17:29:09 -0400

    Implement CalculateJacobian() I wrote in Lesson 5.18.

--- src/tools.cpp
+++ src/tools.cpp
@@ -18,8 +18,29 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+
+    MatrixXd Hj(3,4);
+    //recover state parameters
+    double px = x_state(0);
+    double py = x_state(1);
+    double vx = x_state(2);
+    double vy = x_state(3);
+
+    //compute the Jacobian matrix
+    double px2_py2 = pow(px, 2) + pow(py, 2);
+    double px2_py2_32 = sqrt(pow(px2_py2, 3));
+    double vx_py = vx * py;
+    double vy_px = vy * px;
+
+    //check division by zero-ish
+    if (fabs(px2_py2) < 0.0001) {
+        cout << "TODO: Division by Zero";
+    }
+
+    // Implement all the derivatives into the matrix.
+    Hj << px / sqrt(px2_py2),                py / sqrt(px2_py2),                0,                  0,
+          - py / px2_py2,                    px / px2_py2,                      0,                  0,
+          py * (vx_py - vy_px) / px2_py2_32, py * (vy_px - vx_py) / px2_py2_32, px / sqrt(px2_py2), py / sqrt(px2_py2);
+
+    return Hj;
 }
